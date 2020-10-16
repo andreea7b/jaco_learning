4f51034d3bf2e7b20e61bddf37f9330f32ced132
matthewzurek <matthewzurek1@gmail.com>
2020-10-16 13:00:02 -0400

    Some socket magic

--- src/planning_server.py
+++ src/planning_server.py
@@ -30,6 +30,7 @@ class PlanningServer():
 
 		# setup socket
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		sock.bind(('localhost', PORT_NUM))
 
 		# wait for planning queries
